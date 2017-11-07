d68ff2e0d7c70d88a279ace86100e512514d95b5
Silvio <silvio0407@gmail.com>
2017-11-07 14:14:08 -0200

    Updating data

--- script.sql
+++ script.sql
@@ -4,7 +4,7 @@ USE PARSE_LOG
  
 CREATE TABLE log_informations (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    data_request DATETIME,
+    date_request DATETIME,
     ip varchar(15),
 	description_request varchar(100),
     system_information varchar(500),
